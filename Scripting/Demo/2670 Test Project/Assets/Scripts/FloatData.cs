8eae464b93914b5c22dcead87209696e782ffe3b
Stephen <10559104@my.uvu.edu>
2019-08-28 13:44:47 -0600

    TestProjectScripts

---
+++ Scripting/Demo/2670 Test Project/Assets/Scripts/FloatData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class FloatData : ScriptableObject
+{
+    public float value = 1f;
+
+    public void UpdateValue(float amount)
+    {
+        value += amount;
+    }
+}
---
+++ Scripting/Demo/2670 Test Project/Assets/Scripts/TriggerEventBehavior.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehavior : MonoBehaviour
+{
+    public UnityEvent TriggerEnterEvent, TriggerStayEvent;
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerEnterEvent.Invoke();   
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        TriggerStayEvent.Invoke();
+    }
+}
