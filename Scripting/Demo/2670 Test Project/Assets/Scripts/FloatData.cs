01d5a254637c780da57d832c1f7ba9976b4bd649
Stephen <10559104@my.uvu.edu>
2019-09-04 14:23:24 -0600

    Script Test Unity Events/Components

--- Scripting/Demo/2670 Test Project/Assets/Scripts/FloatData.cs
+++ Scripting/Demo/2670 Test Project/Assets/Scripts/FloatData.cs
@@ -8,8 +8,40 @@ public class FloatData : ScriptableObject
 {
     public float value = 1f;
 
+    public float maxValue = 1f;
+
     public void UpdateValue(float amount)
     {
         value += amount;
     }
+
+    public void UpdateValueLimitZero(float amount)
+    {
+        if (value < 0)
+        {
+            value = 0;
+        }
+        else
+        {
+            UpdateValue(amount);
+        }
+    }
+
+    public void UpdateValueLimitZeroAndMax(float amount)
+    {
+        if (value >= 0 && value <= maxValue)
+        {
+            
+            if (value < maxValue)
+            {
+                value = maxValue;  
+            }
+            else
+            {
+                UpdateValue(value);
+            }
+            UpdateValueLimitZero(amount);
+        }
+    }
+    
 }
---
+++ Scripting/Demo/2670 Test Project/Assets/Scripts/ImageControler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+
+public class ImageController : MonoBehaviour
+{
+    private Image imageComponent;
+    public UnityEvent UpdateImageEvent;
+    private void Start()
+    {
+        imageComponent = GetComponent<Image>();
+    }
+
+    public void UpdateImageComponent(FloatData dataObj)
+    {
+        imageComponent.fillAmount = dataObj.value;
+    }
+    private void Update()
+    {
+        UpdateImageEvent.Invoke();
+    }
+}
