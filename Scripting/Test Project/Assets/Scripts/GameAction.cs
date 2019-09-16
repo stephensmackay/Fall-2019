792a7e7b067adc1fae3cd3b30cecd85269479c5d
StephenSMackay <10559104@my.uvu.edu>
2019-09-15 23:04:42 -0600

    Unity Troubleshooting

---
+++ Scripting/Test Project/Assets/Scripts/GameAction.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+[CreateAssetMenu]
+
+public class GameAction : ScriptableObject
+{
+   public UnityAction action;
+
+   public void raise()
+   {
+       action?.Invoke();
+   }
+}
+
