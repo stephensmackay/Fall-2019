56b9dd8e46ba561a2161d272769163b8b31bcae1
StephenSMackay <10559104@my.uvu.edu>
2019-09-23 14:47:00 -0600

    Basic Character Controller Script

---
+++ Scripting/Test Project/Assets/Scripts/IntData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+ 
+    public int value = 0, maxValue =2;
+
+    public void UpdatetoMaxValue()
+    {
+        value = maxValue;
+    }
+
+    public void UpdatetoNumber(int number)
+    {
+        value = number; 
+    }
+
+
+}
