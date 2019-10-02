d8c74f63538ff2976d65112a15b878b420894a29
Stephen <10559104@my.uvu.edu>
2019-10-02 16:36:36 -0600

    UV Tunnel/Co routines Scrips

---
+++ Scripting/2670Fall2019/2670Fall2019-master/2670 Scripting/Assets/Scripts/CoRoutines.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoRoutines : MonoBehaviour
+{
+
+	public Vector3 location;
+	
+	IEnumerator Start ()
+	{
+		yield return new WaitForSeconds(2);
+
+		transform.position += Vector3.forward;
+	}
+	
+	void Update () {
+		
+	}
+}
