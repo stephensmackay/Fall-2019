31cc631052710837ececb7dafdcf1a824ce6d354
Stephen <10559104@my.uvu.edu>
2019-10-28 14:32:27 -0600

    MoveController

---
+++ Scripting/Tower Defense/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class CharacterMover : ScriptableObject, IMove
+{
+    public Vector3 location { get; set; }
+    public Vector3 newLoc;
+    public float movementSpeed = 1;
+    public void Move(CharacterController controller)
+    {
+        newLoc.x = Input.GetAxis("Horizontal")*(movementSpeed/20);
+        newLoc.z = Input.GetAxis("Vertical")*(movementSpeed/20);
+        controller.Move(newLoc);
+    }
+}
---
+++ Scripting/Tower Defense/Assets/Scripts/MoveController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class MoveController : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public ScriptableObject mover;
+
+    private IMove iMover;
+
+    private int i;
+
+    public ScriptableObject[] movers;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        iMover = mover as IMove;
+    }
+
+    public void ChangeMover()
+    {
+        if (i < movers.Length -1)
+        {
+            i++;
+            iMover = movers[i] as IMove;
+        }
+        else
+        {
+            i = 0;
+            iMover = movers[i] as IMove;
+        }
+    }
+
+    void Update()
+    {
+        iMover.Move(controller);
+    }
+}
+
