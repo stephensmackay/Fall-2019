c042cb6afa3ac83dc857d4556ce11d653fbeb46b
Stephen <10559104@my.uvu.edu>
2019-10-30 16:47:40 -0600

    Update Scene

---
+++ Scripting/Tower Defense/Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class CharacterBase : ScriptableObject
+{
+    public CharacterController Controller { get; set; }
+
+    protected Vector3 Location;
+    protected Vector3 Orientation;
+    public float speed = 10f;
+    public float orientSpeed = 3f;
+    public abstract void Move();
+
+}
---
+++ Scripting/Tower Defense/Assets/Scripts/CharacterStandard.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class CharacterStandard : CharacterBase
+{
+    public override void Move()
+    {
+        Location.Set(newX: Input.GetAxis("Vertical") * speed, newY: 0, newZ: 0);       
+        Orientation.y = Input.GetAxis("Horizontal") * orientSpeed;
+
+        Controller.transform.Rotate(Orientation);
+        Location = Controller.transform.TransformDirection(Location);
+        
+        Controller.Move(Location * Time.deltaTime);
+    }
+}
