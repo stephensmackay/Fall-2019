df6fa834d1526c82855cefa50210b4bfae30c16e
Stephen <10559104@my.uvu.edu>
2019-11-13 14:20:58 -0700

    coinscript test

---
+++ Scripting/Tower Defense/Assets/Scripts/Coin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class Coin : ScriptableObject
+{
+    public UnityAction<int> coinAction;
+
+    public int value = 10;
+
+    public void Spend()
+    {
+        coinAction?.Invoke(value);
+    }
+}
---
+++ Scripting/Tower Defense/Assets/Scripts/Collections.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Collections : ScriptableObject
+{
+    public List<Coin> coinList;
+
+    public void AddCoin(Coin coinObj)
+    {
+        if (!coinList.Contains(coinObj))
+        {
+            coinList.Add(coinObj);
+        }
+    }
+
+    public void UseCoin()
+    {
+        coinList[0].Spend();
+        coinList.RemoveAt(0);
+    }
+
+    public void UseCoinWithValue(int value)
+    {
+        var number = 5;
+        for (var index = 0; index < coinList.Count; index++)
+        {
+            var coin = coinList[index];
+            if (coin.value <= number) continue;
+            coin.Spend();
+            coinList.Remove(coin);
+        }
+    }
+
+    public void UseAllCoins()
+    {
+        foreach (var coin in coinList)
+        {
+            coin.Spend();
+        }
+
+        coinList.Clear();
+    }
+
+    public void UpgradeAllCoins(int value)
+    {
+        foreach (var coin in coinList)
+        {
+            coin.value = value;
+        }
+    }
+}
